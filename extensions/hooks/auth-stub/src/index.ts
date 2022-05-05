0729db6b639db0dd72f6073ad644168bd632fce5
Adam Thomas <adam@teselagen.com>
2022-05-05 19:57:54 -0400

    testing using env vars to selectively disable extensions

--- extensions/hooks/auth-stub/src/index.ts
+++ extensions/hooks/auth-stub/src/index.ts
@@ -2,22 +2,24 @@ import { defineHook } from "@directus/extensions-sdk";
 import { upperCaseName } from "./upperCaseName";
 
 export default defineHook(({ filter, action }) => {
-  filter("items.create", (item) => {
-    console.log("Creating an Item in Directus 9!");
-    console.log(item);
-    // console.log(...args);
-    // if (item && item.name) {
-    //   item.name = item.name.toUpperCase();
-    // }
-    upperCaseName(item);
-  });
+  if (!process.env.DISABLE_AUTH_STUB_EXT) {
+    filter("items.create", (item) => {
+      console.log("Creating an Item in Directus 9!");
+      console.log(item);
+      // console.log(...args);
+      // if (item && item.name) {
+      //   item.name = item.name.toUpperCase();
+      // }
+      upperCaseName(item);
+    });
 
-  action("items.create", (item) => {
-    console.log("Item created!");
-    console.log(item);
-  });
+    action("items.create", (item) => {
+      console.log("Item created!");
+      console.log(item);
+    });
 
-  action("items.read", (item) => {
-    console.log("Collection read " + item?.collection);
-  });
+    action("items.read", (item) => {
+      console.log("Collection read " + item?.collection);
+    });
+  }
 });
